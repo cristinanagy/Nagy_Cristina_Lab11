9b8a6ac48a359f104cd47ca12a9065f151c5f1d5
cristinanagy <58230925+cristinanagy@users.noreply.github.com>
2021-01-09 21:37:01 +0200

    Add project files.

---
+++ PersonalAgenda/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PersonalAgenda.Data;
+using System.IO;
+using Xamarin.Forms;
+
+namespace PersonalAgenda
+{
+    public partial class App : Application {
+       public static AgendaDatabase Database {
+            get {
+                if(database == null) {
+                   database = new
+                   AgendaDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new AgendaPage());
+        }
+
+    }
+}
---
+++ PersonalAgenda/Models/Agenda.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+
+namespace PersonalAgenda.Models {
+   public class Agenda {
+        [PrimaryKey, AutoIncrement]
+        public int ID {
+            get;
+            set;
+        }
+
+        public String Text {
+            get;
+            set;
+        }
+
+        public DateTime Date {
+            get;
+            set;
+        }
+    }
+}
